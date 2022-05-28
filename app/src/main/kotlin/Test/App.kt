902fa203fcd5af32a7e1fca1d1714d50235d67ea
Flaxar <filipschwarz007@gmail.com>
2022-05-28 20:17:27 +0200

    test.html succesfully served

--- app/src/main/kotlin/Test/App.kt
+++ app/src/main/kotlin/Test/App.kt
@@ -17,13 +17,18 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.configureRouting() {
     routing {
-        static("/static") {
+
+        static("/") {
             resources("files")
+        }
+
+        get("/") {
+            call.respondRedirect("/test.html")
         }
     }
 }
 
 fun Application.module() {
     configureRouting()
-    configureTemplating()
+    // configureTemplating()
 }
